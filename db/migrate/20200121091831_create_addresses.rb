19263126c64049e28de522bc944789b08dc1e466
kouhei-masuda <kohei.masuda@di-v.co.jp>
2020-01-21 18:57:11 +0900

    edit_create_address.rb

--- db/migrate/20200121091831_create_addresses.rb
+++ db/migrate/20200121091831_create_addresses.rb
@@ -1,7 +1,12 @@
 class CreateAddresses < ActiveRecord::Migration[5.2]
   def change
     create_table :addresses do |t|
-
+      t.string :postal_code , default: ""
+      t.integer :prefecture , default: 0
+      t.string :city , default: ""
+      t.string :block_number , default: ""
+      t.string :building , default: ""
+      t.reference :user , forign_key: true
       t.timestamps
     end
   end
